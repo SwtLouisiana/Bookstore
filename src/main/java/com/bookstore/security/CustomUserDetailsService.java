797c120cece8305eb644c5bd13da188bcdde2317
Pavlo Sukhanko <pashasyx@gmail.com>
2025-09-09 22:30:27 +0100

    fix comments

--- src/main/java/com/bookstore/security/CustomUserDetailsService.java
+++ src/main/java/com/bookstore/security/CustomUserDetailsService.java
@@ -15,7 +15,8 @@ public class CustomUserDetailsService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         return userRepository.findByEmail(email)
-                .orElseThrow(() -> new UsernameNotFoundException("Can't find user by email"));
+                .orElseThrow(() ->
+                        new UsernameNotFoundException("Can't find user by email: " + email));
     }
 }
 
